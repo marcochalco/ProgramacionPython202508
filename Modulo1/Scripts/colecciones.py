33663b6c05d11eee85d430192c99d471447a711f
marcochalco <chalco.2795@gmail.com>
2025-10-04 20:10:41 +0000

    actualizacion

---
+++ Modulo1/Scripts/colecciones.py
@@ -0,0 +1,24 @@
+"""
+Alamacenar 10 valores
+
+number_1
+number_2
+number_3
+
+"""
+#Colecciones en Python
+
+# Listas 
+# Me permiten alamcenar un conjunto de datos.
+
+lista_numeros = [10, 20, 30, 40, 50]
+
+# tuplas
+tupla_numero = (10, 20, 30, 40, 50)
+
+# Conjuntos
+conjuntos_numeros = {10, 20, 30, 40, 50}
+
+# Diccionarios
+#dict(key, valor)
+
